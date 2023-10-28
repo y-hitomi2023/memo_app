af3ebe45d7635fa2e738df4d904f4a78a6cf6940
y-hitomi2023 <as8stream@gmail.com>
2023-10-28 18:25:07 +0900

    登録機能作成

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -14,6 +14,27 @@ class MemoController extends Controller
         return view('memos.index', ['memos' => $memos]);
     }
 
+    public function create()
+    {
+        return view('memos.create');
+    }
+
+    public function store(Request $request)
+    {
+        // インスタンスの作成
+        $memo = new Memo;
+
+        // 値の用意
+        $memo->title = $request->title;
+        $memo->body = $request->body;
+
+        // インスタンスに値を設定して保存
+        $memo->save();
+
+        // 登録したらindexに戻る
+        return redirect(route("memos.index"));
+    }
+
     public function show($id)
     {
         $memo = Memo::find($id);
--- resources/views/memos/index.blade.php
+++ resources/views/memos/index.blade.php
@@ -1,21 +1,24 @@
-  <!DOCTYPE html>
-  <html lang="ja">
+<!DOCTYPE html>
+<html lang="ja">
 
-  <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>memo index</title>
-  </head>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>memo index</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
 
-  <body>
-      <h1>タイトル一覧</h1>
-      <ul>
-          @foreach ($memos as $memo)
-              <!-- // リンク先をidで取得し名前で出力 -->
-              <li><a href="{{ route('memos.show', $memo) }}">{{ $memo->title }}</a></li>
-          @endforeach
-      </ul>
-  </body>
+<body>
+    <h1>タイトル一覧</h1>
+    <ul>
+        @foreach ($memos as $memo)
+            <!-- // リンク先をidで取得し名前で出力 -->
+            <li><a href="{{ route('memos.show', $memo) }}">{{ $memo->title }}</a></li>
+        @endforeach
+    </ul>
+    <!-- 新規登録画面へジャンプする -->
+    <button onclick="location.href='{{ route('memos.create') }}'">登録する</button>
+</body>
 
-  </html>
+</html>

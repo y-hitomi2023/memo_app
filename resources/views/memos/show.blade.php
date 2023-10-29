fcf725d4012fc6737ed58394caf3b2ff85532bf7
y-hitomi2023 <as8stream@gmail.com>
2023-10-29 10:31:46 +0900

    削除機能完成

--- resources/views/memos/show.blade.php
+++ resources/views/memos/show.blade.php
@@ -1,21 +1,29 @@
-  <!DOCTYPE html>
-  <html lang="ja">
+<!DOCTYPE html>
+<html lang="ja">
 
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>memo show</title>
-  </head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>memo show</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
 
-  <body>
-      {{-- <h1>タイトル</h1> --}}
-      <a href="{{ route('memos.index') }}">戻る</a>
-      <h1>{{ $memo->title }}</h1>
-      {{-- <p>本文</p> --}}
-      {{-- <p>{{ $memo->body }}</p> --}}
-      <p>{!! nl2br(e($memo->body)) !!}</p>
+<body>
+    {{-- <h1>タイトル</h1> --}}
+    <a href="{{ route('memos.index') }}">戻る</a>
+    <h1>{{ $memo->title }}</h1>
+    {{-- <p>本文</p> --}}
+    {{-- <p>{{ $memo->body }}</p> --}}
+    <p>{!! nl2br(e($memo->body)) !!}</p>
 
-      <button onclick='location.href="{{ route("memos.edit", $memo) }}"'>編集する</button>
-  </body>
+    <div class="button-group">
+        <button onclick='location.href="{{ route("memos.edit", $memo) }}"'>編集する</button>
+        <form action="{{ route('memos.destroy', $memo) }}"method="post">
+            @csrf
+            @method('DELETE')
+            <input type="submit" value="削除する" onclick="if(!confirm('削除しますか？')){return false};">
+        </form>
+    </div>
+</body>
 
-  </html>
+</html>
